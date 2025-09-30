272e7ea3185f749b65d4cf8278bf195c5762b5ea
Raneem Hamarneh <raneem99hamarneh@gmail.com>
2025-09-30 11:16:57 +0300

    edit the urls.py in miniTodoApp fixes #1

--- miniTodoApp/urls.py
+++ miniTodoApp/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# from django.http import HttpRequest
 from django.http import HttpResponse
 
 def home(request):
